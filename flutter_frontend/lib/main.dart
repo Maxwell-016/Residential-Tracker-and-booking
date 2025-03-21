97252d9d9424687f7b84932de3c9be9e3ae070a6
Nick Dieda <nickeagle888@gmail.com>
2025-03-21 14:21:30 +0300

    "added app direct to main to prevent entry without login and to direct to respective dash"

--- flutter_frontend/lib/main.dart
+++ flutter_frontend/lib/main.dart
@@ -5,10 +5,19 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_frontend/View-Model/navigation/routes.dart';
 import 'package:flutter_frontend/constants.dart';
+import 'package:flutter_frontend/services/firebase_services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
 import 'View-Model/view_model.dart';
+import 'View/Screens/Admin/admin_dash.dart';
+import 'View/Screens/Common/email_verification_page.dart';
+import 'View/Screens/Common/forgot_password.dart';
+import 'View/Screens/Common/login_page.dart';
+import 'View/Screens/Common/registration_page.dart';
+import 'View/Screens/Landlord/landloard_dash.dart';
+import 'View/Screens/Landlord/manage_house_listings.dart';
+import 'View/Screens/Student/student_dash.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
@@ -57,12 +66,22 @@ class _StateResidentialTrackerAndBooking extends   ConsumerState<ResidentialTrac
 
 
 
+
+
+
 //call this to change the theme of the app (dark or light)
   void changeThemeMode(bool useLightMode){
     setState(() {
       themeMode=useLightMode ?ThemeMode.light:ThemeMode.dark;
     });
   }
+
+
+
+
+
+
+
 
   //use it to change the color of the page /you can add any color in the constants.dart
   void changeColor(int value){
@@ -72,33 +91,133 @@ class _StateResidentialTrackerAndBooking extends   ConsumerState<ResidentialTrac
   }
 
 
-  // Future<String?> _appRedirect(
-  //     BuildContext context, GoRouterState state) async {
-  //   final userDao = ref.watch(userDaoProvider);
-  //   final loggedIn=userDao.isLoggedIn();
-  //
-  //
-  //   // final loggedIn = await _auth.loggedIn;
-  //   final isOnLoginPage = state.matchedLocation == '/login';
-  //
-  //
-  //
-  //   // Go to /login if the user is not signed in
-  //   if (!loggedIn) {
-  //     return '/login';
-  //   }
-  //
-  //
-  //   else if (loggedIn && isOnLoginPage) {
-  //     return '/studentdashboard';
-  //
-  //   }
-  //
-  //   // no redirect
-  //   return null;
-  //
-  //
-  // }
+
+
+
+
+
+   GoRouter routeMaker(BuildContext context) {
+     double deviceWidth = MediaQuery
+         .of(context)
+         .size
+         .width;
+     return GoRouter(
+       initialLocation: '/login',
+       redirect: _appRedirect,
+
+       routes: [
+         GoRoute(
+             builder: (context, state) => LoginPage(),
+             path: '/login'),
+         GoRoute(
+             builder: (context, state) => RegistrationPage(),
+             path: '/registration'),
+
+         GoRoute(
+             builder: (context, state) {
+               return LayoutBuilder(builder: (context, constraints) {
+                 if (constraints.maxWidth > 800) {
+                   return ForgotPassword(
+                     width: deviceWidth / 2,
+                   );
+                 } else {
+                   return ForgotPassword(
+                     width: deviceWidth / 1.1,
+                   );
+                 }
+               });
+             },
+             path: '/forgot-password'),
+         GoRoute(
+             builder: (context, state) {
+               return LayoutBuilder(builder: (context, constraints) {
+                 if (constraints.maxWidth > 800) {
+                   return EmailVerificationPage(
+                     width: deviceWidth / 2,
+                   );
+                 } else {
+                   return EmailVerificationPage(
+                     width: deviceWidth / 1.1,
+                   );
+                 }
+               });
+             },
+             path: '/verification'),
+         GoRoute(
+             builder: (context, state) {
+               return StudentDashboardScreen();
+             },
+             path: '/student-dashboard'),
+         GoRoute(
+             builder: (context, state) {
+               return LandLoardDashboardScreen();
+             },
+             path: '/landlord-dashboard'),
+         GoRoute(
+             builder: (context, state) {
+               return ManageHouseListings();
+             },
+             path: '/manageListings'),
+
+
+         GoRoute(
+             builder: (context, state) {
+               return AdminDashboardScreen();
+             },
+             path: '/admin-dashboard')
+       ],
+       errorPageBuilder: (context, state) {
+         return MaterialPage(
+           key: state.pageKey,
+           child: Scaffold(
+             body: Center(
+               child: Text("404 error:${state.error}"),
+             ),
+           ),
+         );
+       },
+     );
+   }
+
+  Future<String?> _appRedirect(
+      BuildContext context, GoRouterState state) async {
+    final fb = ref.watch(firebaseServices);
+    final loggedIn = fb.loggedIn();
+
+
+    // final loggedIn = await _auth.loggedIn;
+    final isOnLoginPage = state.matchedLocation == '/login';
+
+
+    // Go to /login if the user is not signed in
+    if (!loggedIn) {
+      return '/login';
+    }
+
+    else if (loggedIn && isOnLoginPage) {
+      String? role = await fb.getUserRole();
+      if (role != null) {
+        switch (role) {
+          case 'Student':
+            return '/student-dashboard';
+          case 'Landlord':
+            return '/landlord-dashboard';
+          case 'Admin':
+            return '/admin-dashboard';
+        }
+      }
+      else {
+        return '/login';
+      }
+    }
+      // no redirect
+      return null;
+
+  }
+
+
+
+
 
 
   // This widget is the root of your application.
@@ -119,8 +238,15 @@ class _StateResidentialTrackerAndBooking extends   ConsumerState<ResidentialTrac
       brightness: Brightness.dark
     ),
 
-      routerConfig: Routes.routeMaker(context),
+      routerConfig: routeMaker(context),
+
+
+
+
+
       title: "Residential Tracker and Booking",
     );
   }
 }
+
+
