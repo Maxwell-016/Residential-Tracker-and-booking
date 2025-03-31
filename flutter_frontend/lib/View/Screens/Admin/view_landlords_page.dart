e4ab15fb3d860e694e09d372b3a89ad75abf9ae5
dataNerd65 <pkiarie811@gmail.com>
2025-03-31 14:43:39 +0300

    Added some page to view landlords

---
+++ flutter_frontend/lib/View/Screens/Admin/view_landlords_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ViewLandlordsPage extends StatelessWidget {
+  const ViewLandlordsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('View Landlords')),
+      body: const Center(child: Text('View Landlords Page')),
+    );
+  }
+}
